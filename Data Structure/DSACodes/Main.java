b3d861c4cb5bbe91dff66f807a007a36ce0b9d26
SanjeevGaikwad <gaikwadsanjeevv@gmail.com>
2025-10-06 15:47:37 -0700

    Stack Prog

---
+++ Data Structure/DSACodes/Main.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack <String> stack = new Stack<String>();
+
+        stack.push("Sanjeev");
+        stack.push("Gaikwad");
+        stack.push("Nashik");
+        stack.push("USA");
+        stack.push("USA");
+        stack.push("USA");
+
+        System.out.println(stack.empty());
+        System.out.println(stack.search("USA"));
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+     }
+}
